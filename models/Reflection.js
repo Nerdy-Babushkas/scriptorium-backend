3c65c35295973ea68e27b196db8a2f4f9048e7b4
Maksym Syromolot <msyromolot@myseneca.ca>
2026-03-15 01:09:16 -0400

    feat(api): add reflection title

--- models/Reflection.js
+++ models/Reflection.js
@@ -20,28 +20,29 @@ const progressSchema = new mongoose.Schema(
 
 const reflectionSchema = new mongoose.Schema(
   {
-    // 🔹 Reference to User (ObjectId is correct here)
     user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
       index: true,
     },
-
-    // MUST match Book._id and Track._id (which are Strings)
     item: {
       type: String,
       required: true,
       index: true,
     },
-
     itemType: {
       type: String,
       enum: ["book", "track", "movie"],
       required: true,
       index: true,
     },
-
+    title: {
+      type: String,
+      trim: true,
+      maxlength: 200, // optional limit
+      default: "", // optional default
+    },
     text: {
       type: String,
       required: true,
@@ -49,26 +50,19 @@ const reflectionSchema = new mongoose.Schema(
       maxlength: 2000,
       trim: true,
     },
-
     moodTags: {
       type: [String],
       default: [],
     },
-
     feelings: {
       type: [String],
       default: [],
     },
-
     progress: progressSchema,
-
-    // User-selected reflection date (not creation date)
     date: {
       type: Date,
       default: Date.now,
     },
-
-    // Optional flexible data (location, weather, etc.)
     metadata: {
       type: mongoose.Schema.Types.Mixed,
       default: {},
