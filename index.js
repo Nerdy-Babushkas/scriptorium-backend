6aabc602e754de05459f1d81ee2fa6561832077f
harjasdee-singh <harjasdeep-singh@myseneca.ca>
2026-03-26 20:53:57 -0400

    update index.js

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // index.js
 const app = require("./app");
 
-module.exports = app;
+const HTTP_PORT = process.env.PORT || 8080;
+
+app.listen(HTTP_PORT, () => {
+  console.log("API listening on port " + HTTP_PORT);
+});
